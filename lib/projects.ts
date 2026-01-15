f8c90b786e63b1600eec289f92ab9723be82de88
Bradley-Hill <brad.michael.hill@gmail.com>
2026-01-15 15:53:25 +0100

    bradley-hill: added projects list to use for dynamic rendering

---
+++ lib/projects.ts
@@ -0,0 +1,89 @@
+export interface Project {
+  id: string;
+  title: string;
+  description: string;
+  technologies: string[];
+  githubUrl: string;
+  liveUrl?: string;
+  image?: string;
+  featured?: boolean;
+}
+
+export const projects: Project[] = [
+  {
+    id: "rncp-project",
+    title: "Plant care Assisstant",
+    description:
+      "A web app to help users take care of their plants, built as part of my RNCP diploma",
+    technologies: [
+      "React",
+      "TypeScript",
+      "Next.js",
+      "Tailwind CSS",
+      "Docker",
+      "Supabase",
+      "Prisma",
+      "Jest",
+      "Playwright",
+    ],
+    githubUrl: "https://github.com/Bradley-Hill/PlantCareAssistant",
+    liveUrl: "",
+    image: "/images/placeholder.jpg",
+    featured: true,
+  },
+  {
+    id: "shopping-cart",
+    title: "Shopping Cart",
+    description: "E-commerce shopping cart built as part of The Odin Project",
+    technologies: ["React", "TypeScript"],
+    githubUrl: "https://github.com/Bradley-Hill/TheOdinProject-ShoppingCart",
+    liveUrl: "https://bradley-hill.github.io/TheOdinProject-ShoppingCart/",
+    image: "/images/placeholder.jpg",
+  },
+  {
+    id: "navigator-battleship",
+    title: "Battleship Game",
+    description: "",
+    technologies: ["React", "TypeScript", "Jest"],
+    githubUrl: "https://github.com/Bradley-Hill/navigator-battleship",
+    liveUrl: "",
+    image: "/images/placeholder.jpg",
+  },
+  {
+    id: "knights-travail",
+    title: "Knights Travail Solution",
+    description: "Solution to the Knight Travail problem using OOP principles",
+    technologies: ["JavaScript", "TypeScript"],
+    githubUrl: "https://github.com/Bradley-Hill/KnightsTravail-Graphs",
+    liveUrl: "",
+    image: "/images/placeholder.jpg",
+  },
+  {
+    id: "binary-search-tree",
+    title: "Binary Search Tree Implementation",
+    description: "This project is a simple implementation of a Binary Search Tree (BST) in TypeScript.",
+    technologies: ["TypeScript"],
+    githubUrl: "https://github.com/...",
+    liveUrl: "",
+    image: "/images/placeholder.jpg",
+  },
+  {
+    id: "hashmap-implementation",
+    title: "Hashmap Implementation",
+    description: "This project is a simple implementation of a Hashmap in TypeScript.",
+    technologies: ["TypeScript"],
+    githubUrl: "https://github.com/Bradley-Hill/hashMap",
+    liveUrl: "",
+    image: "/images/placeholder.jpg",
+  },
+  // ... other Odin exercises
+  //   {
+  //     id: "",
+  //     title: "",
+  //     description: "",
+  //     technologies: ["React", "JavaScript"],
+  //     githubUrl: "https://github.com/...",
+  //     liveUrl: "https://...",
+  //     image: "/images/placeholder.jpg",
+  //   },
+];
